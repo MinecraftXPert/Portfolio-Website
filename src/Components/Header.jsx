76cba4621f0a73597e4185cfeffe3632d8c25504
Noah R <noahrang02@gmail.com>
2025-06-27 14:02:31 -0400

    Add icons and underline effect

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import HomeIcon from "../Icons/HomeIcon";
+import AboutIcon from "../Icons/AboutIcon";
+import ProjectsIcon from "../Icons/ProjectsIcon";
+import ContactIcon from "../Icons/ContactIcon";
 
 export default function Header() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
@@ -8,30 +12,41 @@ export default function Header() {
   }
 
   return (
-    <nav className="shadow-sm w-full">
+    <nav className="shadow-sm w-full p-1  sm:p-3">
       {/* navbar */}
-      <ul className="flex sm:justify-around justify-start items-center p-5 ml-5 sm:gap-0 gap-2">
-        <li className="mr-auto text-2xl">
-          <a href="#" className="text-3xl">
+      <ul className="flex sm:justify-around justify-start items-center p-5 ml-10 mr-10 sm:gap-0 gap-2">
+        <li className="sm:ml-1 mr-auto text-2xl">
+          <span className="text-3xl">
             Noah
+          </span>
+        </li>
+        <li className="sm:block hidden px-[25px] py-0 text-[14px]">
+          <a className="flex items-center gap-2" href="#">
+            <HomeIcon /> Home
           </a>
         </li>
-        <li className="sm:block hidden px-[30px] py-0 text-[14px]">
-          <a href="#">Home</a>
+        <li className="sm:block hidden px-[25px] py-0 text-[14px]">
+          <a className="flex items-center gap-2" href="#">
+            <AboutIcon />
+            About
+          </a>
         </li>
-        <li className="sm:block hidden px-[30px] py-0 text-[14px]">
-          <a href="#">About</a>
+        <li className="sm:block hidden px-[25px] py-0 text-[14px]">
+          <a className="flex items-center gap-2" href="#">
+            <ProjectsIcon />
+            Projects
+          </a>
         </li>
-        <li className="sm:block hidden px-[30px] py-0 text-[14px]">
-          <a href="#">Projects</a>
-        </li>
-        <li className="sm:block hidden px-[30px] py-0 text-[14px]">
-          <a href="#">Contact</a>
+        <li className="sm:block hidden px-[25px] py-0 text-[14px]">
+          <a className="flex items-center gap-2" href="#">
+            <ContactIcon />
+            Contact
+          </a>
         </li>
         {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
         <svg
           onClick={toggleSideBar}
-          className="w-[20px] sm:hidden cursor-pointer"
+          className=" w-[20px] sm:hidden cursor-pointer"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
         >
@@ -47,23 +62,35 @@ export default function Header() {
         {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
         <svg
           onClick={toggleSideBar}
-          className="m-5 w-[25px] cursor-pointer"
+          className="mx-5 w-[25px] cursor-pointer my-5"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 384 512"
         >
           <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
         </svg>
         <li className="px-[30px] py-0 text-[14px]">
-          <a href="#">Home</a>
+          <a className="flex items-center gap-3"href="#">
+            <HomeIcon />
+            Home
+          </a>
         </li>
         <li className="px-[30px] py-0 text-[14px]">
-          <a href="#">About</a>
+          <a className="flex items-center gap-3" href="#">
+            <AboutIcon />
+            About
+          </a>
         </li>
         <li className="px-[30px] py-0 text-[14px]">
-          <a href="#">Projects</a>
+          <a className="flex items-center gap-3" href="#">
+            <ProjectsIcon />
+            Projects
+          </a>
         </li>
         <li className="px-[30px] py-0 text-[14px]">
-          <a href="#">Contact</a>
+          <a className="flex items-center gap-3" href="#">
+            <ContactIcon />
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
